1c09014265fe607b8c29dbaf7847616575da4bc3
TheArmedSoldier <mohamadhussein15@hotmail.com>
2022-12-26 15:55:23 -0700

    delete title, add hint, add tweaks

--- app/src/main/java/com/firstapp/chat/MainActivity.java
+++ app/src/main/java/com/firstapp/chat/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,11 +21,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
-        inputText = (EditText) findViewById(R.id.inputText);
+        // Button
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(e -> handleText());
+        //
+        textView = findViewById(R.id.textView);
+        inputText = findViewById(R.id.inputText);
     }
 
-    public void handleText(View v) {
+    public void handleText() {
         textView.setText(inputText.getText());
         System.out.println("Button Clicked");
 

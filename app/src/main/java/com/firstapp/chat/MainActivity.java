bcd858504b94e7038b8931bd125dc6d4f9610e45
TheArmedSoldier <mohamadhussein15@hotmail.com>
2022-12-26 16:36:17 -0700

    set up plugin for using python

--- app/src/main/java/com/firstapp/chat/MainActivity.java
+++ app/src/main/java/com/firstapp/chat/MainActivity.java
@@ -11,6 +11,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
@@ -25,9 +29,21 @@ public class MainActivity extends AppCompatActivity {
         Button btn = findViewById(R.id.button);
         btn.setOnClickListener(e -> handleText());
 
-        // initialkizing
+        // initializing
         textView = findViewById(R.id.textView);
         inputText = findViewById(R.id.inputText);
+
+        // python initialization
+        if (!Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+
+        // create python instance
+        Python py = Python.getInstance();
+
+        //now create python object
+        PyObject pyobj = py.getModule("main.py");
+
     }
 
     public void handleText() {

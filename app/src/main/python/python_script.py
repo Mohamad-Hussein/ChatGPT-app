1006aacfc2e84e69b87c6a9699d5317e54b424d8
TheArmedSoldier <mohamadhussein15@hotmail.com>
2022-12-28 10:00:20 -0700

    add error handling from chatgpt side in py file

--- app/src/main/python/python_script.py
+++ app/src/main/python/python_script.py
@@ -3,6 +3,9 @@ import openai
 def main(user_prompt):
     openai.api_key = ("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")
     filtered_prompt = str(user_prompt)
-    response = openai.Completion.create(model="text-davinci-003", prompt= filtered_prompt, temperature=0, max_tokens=500)
-
-    return response["choices"][0]["text"]
+    try:
+        response = openai.Completion.create(model="text-davinci-003", prompt= filtered_prompt, temperature=0, max_tokens=500)
+    except:
+        return "ChatGPT had an error"
+    else:
+        return response["choices"][0]["text"]

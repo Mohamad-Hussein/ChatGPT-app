72403d3ebc58a5afe6aea1be76f1b09d110aae0c
Mohamad-Hussein <115669425+Mohamad-Hussein@users.noreply.github.com>
2022-12-28 10:02:28 -0700

    Update python_script.py

--- app/src/main/python/python_script.py
+++ app/src/main/python/python_script.py
@@ -1,7 +1,7 @@
 import openai
 
 def main(user_prompt):
-    openai.api_key = ("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")
+    openai.api_key = ("API_KEY")
     filtered_prompt = str(user_prompt)
     try:
         response = openai.Completion.create(model="text-davinci-003", prompt= filtered_prompt, temperature=0, max_tokens=500)
